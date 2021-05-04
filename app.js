54d41c21321f119565e591c6cdb18b91acb5891f
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-03 21:54:33 -1000

    got the first line to work.

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const moveHamburgerIcon = () => {
   const one = document.getElementsByClassName("one")[0];
   const two = document.getElementsByClassName("two")[0];
   const three = document.getElementsByClassName("three")[0];
-  
-  one.classList.add("moveOne");
-  two.classList.add("moveTwo");
-  three.classList.add("moveThree");
+
+  if (isXIcon) {
+    // one.classList.remove("moveOne");
+    one.classList.add("moveOneBack");
+  } else {
+    one.classList.add("moveOne");
+    two.classList.add("moveTwo");
+    three.classList.add("moveThree");
+  }
 
   isXIcon = !isXIcon;
 
