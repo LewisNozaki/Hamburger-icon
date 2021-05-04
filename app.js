32edacbdff00e5e280742a3a05b27e8ef57b76c3
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-03 21:41:38 -1000

    click function works but not the other way around

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 
 const container = document.getElementsByClassName("container")[0];
 
+let isXIcon = false;
+
 const moveHamburgerIcon = () => {
   const one = document.getElementsByClassName("one")[0];
   const two = document.getElementsByClassName("two")[0];
   const three = document.getElementsByClassName("three")[0];
   
-  console.log(one);
-  console.log(two);
-  console.log(three);
+  one.classList.add("moveOne");
+  two.classList.add("moveTwo");
+  three.classList.add("moveThree");
 
+  isXIcon = !isXIcon;
+
+  console.log(isXIcon);
 }
 
 container.addEventListener("click", moveHamburgerIcon);
