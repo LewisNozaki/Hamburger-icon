5c3f929c7a86c1ec5d009a1df4e9f77dae505a97
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-03 22:10:32 -1000

    updated readme file with pictures

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Hamburger-icon
 
 A simple hamburger icon created with HTML and CSS using divs and animates it into an "X" with animations and @keyframes.
+
+The animation is that all three lines will collapse onto the middle line. Then the blue and the green lines will rotate 45/-45 degrees and create the X while the purple line will disappear with an opacity of 0.
+
+Opened:
+
+![opened](open.png)
+
+closed:
+
+![closed](closed.png)
--- app.js
+++ app.js
@@ -8,19 +8,24 @@ const moveHamburgerIcon = () => {
   const one = document.getElementsByClassName("one")[0];
   const two = document.getElementsByClassName("two")[0];
   const three = document.getElementsByClassName("three")[0];
-
+  
   if (isXIcon) {
-    // one.classList.remove("moveOne");
+    one.classList.remove("moveOne");
     one.classList.add("moveOneBack");
+    two.classList.remove("moveTwo");
+    two.classList.add("moveTwoBack");
+    three.classList.remove("moveThree");
+    three.classList.add("moveThreeBack");
   } else {
+    one.classList.remove("moveOneBack");
     one.classList.add("moveOne");
+    two.classList.remove("moveTwoBack");
     two.classList.add("moveTwo");
+    three.classList.remove("moveThreeBack");
     three.classList.add("moveThree");
   }
 
   isXIcon = !isXIcon;
-
-  console.log(isXIcon);
 }
 
 container.addEventListener("click", moveHamburgerIcon);
