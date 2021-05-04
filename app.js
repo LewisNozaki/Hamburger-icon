2ed97ef4e27fde96e87ca61511dc5e727c3ba16b
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-03 21:35:02 -1000

    moveHamburgerIcon function created

--- app.js
+++ app.js
@@ -2,7 +2,18 @@
 
 const container = document.getElementsByClassName("container")[0];
 
-console.log(container);
+const moveHamburgerIcon = () => {
+  const one = document.getElementsByClassName("one")[0];
+  const two = document.getElementsByClassName("two")[0];
+  const three = document.getElementsByClassName("three")[0];
+  
+  console.log(one);
+  console.log(two);
+  console.log(three);
+
+}
+
+container.addEventListener("click", moveHamburgerIcon);
 
 
 
